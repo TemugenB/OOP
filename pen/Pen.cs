64c04250f0636d015190c3e6ad5c5216b722bb9e
TemugenB <168091266+TemugenB@users.noreply.github.com>
2024-05-12 19:24:01 +0200

    Add files via upload

---
+++ pen/Pen.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextFile;
+
+namespace pen
+{
+    public struct Pen
+    {
+        public string name;
+        public string colour;
+        public int amount;
+    }
+    public struct Custom
+    {
+        public string brand;
+        public int total;
+    }
+    public enum Status
+    {
+        norm, abnorm
+    }
+
+    internal class Enumerator
+    {
+       
+        private TextFileReader x;
+        private Pen dx;
+        Status sx;
+        Custom curr;
+        bool end;
+        public Enumerator(string name)
+        {
+            x = new TextFileReader(name);
+        }
+        public void Read() 
+        {
+            if(!(x.ReadString(out dx.name) && x.ReadString(out dx.colour) && x.ReadInt(out dx.amount)))
+            {
+                sx = Status.abnorm;
+            }
+            else
+            {
+                sx = Status.norm;
+            }
+        }
+        public void First()
+        {
+            Read();
+            Next();
+        }
+        public Custom Current()
+        {
+            return curr;
+        }
+        public bool End()
+        {
+            return end;
+        }
+        public void Next()
+        {
+            end = (sx == Status.abnorm);
+            if (!end)
+            {
+                curr.brand = dx.name;
+                curr.total = 0;
+                while (curr.brand == dx.name)
+                {
+                    curr.total += dx.amount;
+                    Read();
+                }
+            }
+        }
+    }
+}
---
+++ pen/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace pen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Enumerator t = new Enumerator("input.txt");
+                t.First();
+                while (!t.End())
+                {
+                    if (t.Current().total > 70)
+                    {
+                        Console.WriteLine($"{t.Current().brand}");
+                    }
+                    t.Next();
+                }
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                Console.WriteLine("File not Found!");
+            }
+        }
+    }
+}
